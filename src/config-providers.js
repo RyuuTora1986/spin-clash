5c1efafbd86655ddec92de1bfcb69e99a6a49f1b
RyuuTora1986 <liuyinzg@gmail.com>
2026-04-18 23:15:24 +0900

    chore: sync release runtime and pages pipeline (57)

---
+++ src/config-providers.js
@@ -0,0 +1,30 @@
+(function(){
+  const root = window.SpinClash || (window.SpinClash = {});
+  root.config = root.config || {};
+
+  root.config.providers = {
+    reward: {
+      adapter: 'mock',
+      mockMode: 'grant',
+      adsense: {
+        enabled: false,
+        scriptUrl: 'https://securepubads.g.doubleclick.net/tag/js/gpt.js',
+        rewardedAdUnitPath: '',
+        gamInterstitialAdUnitPath: ''
+      }
+    },
+    analytics: {
+      adapter: 'local_buffer',
+      enableForwarding: false,
+      posthog: {
+        enabled: false,
+        projectApiKey: '',
+        apiHost: 'https://us.i.posthog.com',
+        scriptUrl: '',
+        capturePageview: false,
+        autocapture: false,
+        disableSessionRecording: true
+      }
+    }
+  };
+})();
